e3210a9e71610f158389336cb67fcae04fa4195d
Sovandara UOM <Sovandara.Uom@jtrustroyal.com>
2022-04-19 08:00:30 +0700

    Initial commit

---
+++ test/cdk-step-functions-event-schedule.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStepFunctionsEventSchedule from '../lib/cdk-step-functions-event-schedule-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-step-functions-event-schedule-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStepFunctionsEventSchedule.CdkStepFunctionsEventScheduleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
